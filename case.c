dc8aaf083faca191a585c9d0d19d73ed807f39c7
Kalebe Vasconcelos <kalebevasconcelos099@gmail.com>
2023-01-30 09:44:12 -0300

    Create case.c

---
+++ case.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <conio2.h>
+ 
+int main(){
+	int valor;
+	
+	printf("Digite um valor de 1 a 3: ");
+	scanf("%d", &valor);
+	
+	switch(valor){
+		case 1:
+			printf("Domingo\n");
+			break;
+		
+		case 2:
+			printf("Segunda\n");
+			break;
+		
+		case 3:
+			printf("Segunda\n");
+		break;
+		
+		default:
+			printf("Valor invalido!\n");
+	}
+	getch();
+	return 0;
+}
